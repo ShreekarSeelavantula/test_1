269360318d846e0bf03a216c1046646da8aeba8d
Shreekar <krishnakumar1572004@gmail.com>
2025-04-21 15:22:53 +0530

    i am trying cpp file with return type as null

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -8,21 +8,7 @@ struct StudentNode {
 };
 
 // Function to add students to the linked list and return the head of the list
-StudentNode* runRollCallOperations(int rollNumbers[], int count) {
-    if (count == 0) {
-        return nullptr; // Return empty list if no students
-    }
-
-    // Create the head node
-    StudentNode* head = new StudentNode(rollNumbers[0]);
-    StudentNode* current = head;
-
-    // Iterate through remaining roll numbers and create nodes
-    for (int i = 1; i < count; ++i) {
-        current->next = new StudentNode(rollNumbers[i]);
-        current = current->next;
-    }
-
-    return head; // Return the head of the linked list
+StudentNode* runRollCallOperations(int rollNumbers[], int count) {//
+    return nullptr;
+   
 }
-
