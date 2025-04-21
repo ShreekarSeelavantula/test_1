48e7bcf8f9166c5dc8a5a62c0f5b5e9142a503c4
Shreekar <krishnakumar1572004@gmail.com>
2025-04-21 15:20:07 +0530

    i am trying the cpp file for 2nd time with solution

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -8,8 +8,21 @@ struct StudentNode {
 };
 
 // Function to add students to the linked list and return the head of the list
-StudentNode* runRollCallOperations(int rollNumbers[], int count) {//
+StudentNode* runRollCallOperations(int rollNumbers[], int count) {
+    if (count == 0) {
+        return nullptr; // Return empty list if no students
+    }
 
-    return nullptr;
-   
+    // Create the head node
+    StudentNode* head = new StudentNode(rollNumbers[0]);
+    StudentNode* current = head;
+
+    // Iterate through remaining roll numbers and create nodes
+    for (int i = 1; i < count; ++i) {
+        current->next = new StudentNode(rollNumbers[i]);
+        current = current->next;
+    }
+
+    return head; // Return the head of the linked list
 }
+
