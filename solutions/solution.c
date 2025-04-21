e2f4620a69e2b714f9e195b828e4b2460ccb835e
Shreekar <krishnakumar1572004@gmail.com>
2025-04-21 14:47:30 +0530

    trying the c file for third time

--- solutions/solution.c
+++ solutions/solution.c
@@ -8,6 +8,30 @@ struct StudentNode {
 };
 
 // Function to add students to the list and return the head of the list
-struct StudentNode* runRollCallOperations(const int* rollNumbers, int count) {
-    ////
-}
+
+
+
+
+    struct StudentNode* runRollCallOperations(const int* rollNumbers, int count) {
+        if (count == 0) return NULL;
+    
+        struct StudentNode* head = (struct StudentNode*) malloc(sizeof(struct StudentNode));
+        head->rollNumber = rollNumbers[0];
+        head->next = NULL;
+    
+        struct StudentNode* current = head;
+        for (int i = 1; i < count; i++) {
+            struct StudentNode* newNode = (struct StudentNode*) malloc(sizeof(struct StudentNode));
+            newNode->rollNumber = rollNumbers[i];
+            newNode->next = NULL;
+            current->next = newNode;
+            current = newNode;
+        }
+    
+        return head;
+    }
+
+
+
+
+    
