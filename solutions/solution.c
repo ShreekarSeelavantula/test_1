732b74401204156175575f0bdb3b45c664ab6dc8
Shreekar <krishnakumar1572004@gmail.com>
2025-04-21 14:49:39 +0530

    fifth time checking for c file

--- solutions/solution.c
+++ solutions/solution.c
@@ -13,22 +13,15 @@ struct StudentNode {
 
 
     struct StudentNode* runRollCallOperations(const int* rollNumbers, int count) {
-        if (count == 0) return NULL;
+
+
+
+       
     
-        struct StudentNode* head = (struct StudentNode*) malloc(sizeof(struct StudentNode));
-        head->rollNumber = rollNumbers[0];
-        head->next = NULL;
-    
-        struct StudentNode* current = head;
-        for (int i = 1; i < count; i++) {
-            struct StudentNode* newNode = (struct StudentNode*) malloc(sizeof(struct StudentNode));
-            newNode->rollNumber = rollNumbers[i];
-            newNode->next = NULL;
-            current->next = newNode;
-            current = newNode;
-        }
-    
-        return head;
+        return ;
+
+
+
     }
 
 
