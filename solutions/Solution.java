fa9099a07f590df23c30f6881721af00b62de3d5
Shreekar <krishnakumar1572004@gmail.com>
2025-04-21 15:45:31 +0530

    checking java file with full solution

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -1,9 +1,11 @@
 package solutions;
 
-public class StudentNode {
+// Define the class for the Node in the linked list (without 'public' access modifier)
+class StudentNode {
     int rollNumber;
     StudentNode next;
 
+    // Constructor to initialize the roll number and next node
     StudentNode(int rollNumber) {
         this.rollNumber = rollNumber;
         this.next = null;
@@ -11,15 +13,19 @@ public class StudentNode {
 }
 
 public class Solution {
+
+    // Method to perform roll call operations and return the head of the linked list
     public static StudentNode runRollCallOperations(int[] rollNumbers) {
         StudentNode head = null;
 
+        // Iterate through the roll numbers and create linked list nodes
         for (int rollNumber : rollNumbers) {
             StudentNode newNode = new StudentNode(rollNumber);
             if (head == null) {
                 head = newNode;
             } else {
                 StudentNode current = head;
+                // Traverse to the end of the list
                 while (current.next != null) {
                     current = current.next;
                 }
@@ -29,6 +35,7 @@ public class Solution {
         return head;
     }
 
+    // Method to print the entire linked list
     public static void printList(StudentNode head) {
         StudentNode current = head;
         while (current != null) {
