01b3143ebbdc415362986a51d16d62c1310dd777
shreekar <shreekar157200419@gmail.com>
2025-04-22 13:07:31 +0530

    checking python file with empty code

--- solutions/solution.py
+++ solutions/solution.py
@@ -6,10 +6,4 @@ class StudentNode:
 
 # Function to add students to the linked list and return the head of the list
 def run_roll_call_operations(roll_numbers):
-    head = None
-    for roll_number in reversed(roll_numbers):  # Reverse to maintain the correct order
-        new_node = StudentNode(roll_number)
-        new_node.next = head
-        head = new_node
-    return head
-#@1qqq123
+    return
