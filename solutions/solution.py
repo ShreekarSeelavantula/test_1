c29eadaa44bae4bcb10ef7344df00e4b0f1f99f7
shreekar <shreekar157200419@gmail.com>
2025-04-22 13:22:00 +0530

    checking the python file without return keyword

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,9 +1,7 @@
-# Define the Node class for the linked list
 class StudentNode:
     def __init__(self, roll_number):
         self.roll_number = roll_number
         self.next = None
 
-# Function to add students to the linked list and return the head of the list
 def run_roll_call_operations(roll_numbers):
-    #q
+    #q2
