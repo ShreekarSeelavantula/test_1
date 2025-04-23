bd11bb41876e3f13feb65a965580109b3ff2304f
shreekar <shreekar157200419@gmail.com>
2025-04-23 10:59:37 +0530

    checking the java file without return statement

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -12,11 +12,7 @@ public class Solution {
         }
     }
 
-    /**
-     * Creates a linked list from an array of roll numbers in the same order.
-     * @param rollNumbers array of roll numbers
-     * @return head of the linked list
-     */
+   
     public static StudentNode runRollCallOperations(int[] rollNumbers) {
 
        
