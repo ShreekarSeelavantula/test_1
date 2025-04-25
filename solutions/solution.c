0402c6cbbeaf4d80fe2cbf48abb2e470f89316f4
shreekar <shreekar157200419@gmail.com>
2025-04-25 14:49:21 +0530

    checking the c file

--- solutions/solution.c
+++ solutions/solution.c
@@ -9,20 +9,5 @@ struct StudentNode {
 
 // Function to create a linked list from an array of roll numbers
 struct StudentNode* runRollCallOperations(const int rollNumbers[], int count) {
-    if (count == 0) return NULL;
-
-    struct StudentNode* head = (struct StudentNode*) malloc(sizeof(struct StudentNode));
-    head->rollNumber = rollNumbers[0];
-    head->next = NULL;
-
-    struct StudentNode* current = head;
-    for (int i = 1; i < count; ++i) {
-        struct StudentNode* newNode = (struct StudentNode*) malloc(sizeof(struct StudentNode));
-        newNode->rollNumber = rollNumbers[i];
-        newNode->next = NULL;
-        current->next = newNode;
-        current = newNode;
-    }
-
-    return head;
+    //write the code and return the head
 }
