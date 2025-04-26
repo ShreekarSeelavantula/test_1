45cb6eb845ccedaed83de1388f2e6fe1a82cd1ba
shreekar <shreekar157200419@gmail.com>
2025-04-26 13:37:35 +0530

    checking with new yml file

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -14,12 +14,14 @@ public class Solution {
 
     public static StudentNode runRollCallOperations(int[] rollNumbers) {
         if (rollNumbers == null || rollNumbers.length == 0) {
-            return null;
+            return null;  // Very important: return null for empty input
         }
 
+        // Create the head node
         StudentNode head = new StudentNode(rollNumbers[0]);
         StudentNode current = head;
 
+        // Link the rest of the nodes
         for (int i = 1; i < rollNumbers.length; i++) {
             current.next = new StudentNode(rollNumbers[i]);
             current = current.next;
