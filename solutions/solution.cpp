661f6fc739271fe9abd79ffef73149577b36ef27
shreekar <shreekar157200419@gmail.com>
2025-04-26 12:44:53 +0530

    checking c file

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -11,15 +11,6 @@ struct StudentNode {
 
 // Function to create a linked list from an array of roll numbers
 StudentNode* runRollCallOperations(int rollNumbers[], int count) {
-    if (count == 0) return nullptr; // if no students
-
-    StudentNode* head = new StudentNode(rollNumbers[0]); // create head
-    StudentNode* current = head;
-
-    for (int i = 1; i < count; i++) {
-        current->next = new StudentNode(rollNumbers[i]); // create next node
-        current = current->next; // move to next node
-    }
-
-    return head; // return head of the linked list
+    
+    return nullptr; // return head of the linked list
 }
