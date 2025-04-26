1b26d97d8e86eb2c8354be5e93cff3966290dc73
shreekar <shreekar157200419@gmail.com>
2025-04-26 14:45:24 +0530

    checking with new yml file

--- solutions/solution.py
+++ solutions/solution.py
@@ -4,16 +4,6 @@ class StudentNode:
         self.next = None
 
 def run_roll_call_operations(roll_numbers):
-    if not roll_numbers:  # If the input list is empty, return None
-        return None
+    return None
 
-    # Create the head of the list
-    head = StudentNode(roll_numbers[0])
-    current = head
-
-    # Add nodes for each roll number in the list
-    for roll_number in roll_numbers[1:]:
-        current.next = StudentNode(roll_number)
-        current = current.next
-
-    return head
+   
